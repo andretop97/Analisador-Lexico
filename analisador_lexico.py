e832714fc51f9819fc291f869a1639fba6777297
Andre Luiz Branco Barbosa Araujo <andreluizpsn975@hotmail.com>
2020-09-16 22:59:09 -0300

    Criando classe lexicalAnalyzer

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,26 +1,65 @@
-class tabela_de_simbolos():
+import automato_finito_deterministico as DFA,
+import re
+class SymbolTable():
     def __init__(self):
-        self.simbolo = {}
+        self.symbol = {}
 
-    def add_simbolo(self , token , lexema , tipo):
-        self.simbolo[lexema] = {"lexema": lexema , "token": token , "tipo": tipo}
+    def addSymbol(self , token , lexeme , tipo):
+        self.symbol[lexeme] = {"lexema": lexeme , "token": token , "tipo": tipo }
 
-    def verificacao_existencia_simbolo(self , lexema):
-        if lexema in self.simbolo:
+    def checkSymbolExistence(self , lexeme):
+        if lexeme in self.symbol:
             return True
         else: 
             return False
 
+class lexicalAnalyzer:
+    def __init__(self):
+        self.symbleTable = SymbolTable()
+        self.DFA = DFA.DeterministicFiniteAutomaton(["a" , "b" , "c"],["s0","s1","s2","s3","s4","s5","s6","s7","s8","s9"] , funcao_de_transicao , "s0" , ["s2"])
+
+    def readFile(self , fileName):
+        file = open(fileName,"r")
+
+        for line in file:
+            lexemes = line.split()
+            print(lexemes)
+
+
+def funcao_de_transicao(state , letter):
+    if state == "s0":
+        if re.findall("\d", letter):
+            return "s1"
+        elif re.findall("\w", letter):
+            return "s2"
+        else:
+            return "Serr"
+
+    if state == "s1":
+        if re.findall("\d", letter):
+            return "s1"
+        elif re.findall("\w", letter):
+            return "s2"
+        else:
+            return "err"
+
+    if state =="s2":
+        if re.findall("\w", letter):
+            return "s2"
+        elif re.findall("\d", letter):
+            return "s1"
+        else:
+            return "err"
 
 
 if __name__ == "__main__":
-    c = tabela_de_simbolos()
-    c.add_simbolo("int","int","int")
+    c = SymbolTable()
+    c.addSymbol("int","int","int")
 
     print(c.simbolo)
 
-    d = c.verificacao_existencia_simbolo("int")
-    e = c.verificacao_existencia_simbolo("batata")
+    d = c.checkSymbolExistence("int")
+    e = c.checkSymbolExistence("batata")
     print(d , "  " , e)
 
 
--- automato_finito_deterministico.py
+++ automato_finito_deterministico.py
@@ -4,13 +4,13 @@ class DeterministicFiniteAutomaton:
     def __init__ (self , alphabet , states , transitionFunction , initialState , validStates):
         self.alphabet = alphabet
         self.states = states
-        self.transition_function = transitionFunction
-        self.initial_state = initialState
-        self.valid_states = validStates
+        self.transitionFunction = transitionFunction
+        self.initialState = initialState
+        self.validStates = validStates
 
     # Função que verifica se o estado atual é final valido
     def isValidFinalState(self , state) :
-        if state in self.valid_states:
+        if state in self.validStates:
             return True
         else:
             return False
@@ -23,11 +23,11 @@ class DeterministicFiniteAutomaton:
 
     # Função que recebe estado e o simbolo , para a paritr dele e da função de transição retornar o proximo estado
     def nextState(self , state, symbol):
-       return self.transition_function(state , symbol)
+       return self.transitionFunction(state , symbol)
 
     # Função que analisa a palavra e retorna o estado final da mesma 
     def lexemeVerify(self , lexeme):
-        state = self.initial_state
+        state = self.initialState
         for symbol in lexeme:
             if self.isValidSymbol(symbol):
                state = self.nextState(state , symbol)
@@ -43,37 +43,37 @@ class DeterministicFiniteAutomaton:
 
         
 
-def funcao_de_transicao(state , letter):
-    if state == "s0":
-        if re.findall("\d", letter):
-            return "s1"
-        elif re.findall("\w", letter):
-            return "s2"
-        else:
-            return "Serr"
+# def funcao_de_transicao(state , letter):
+#     if state == "s0":
+#         if re.findall("\d", letter):
+#             return "s1"
+#         elif re.findall("\w", letter):
+#             return "s2"
+#         else:
+#             return "Serr"
 
-    if state == "s1":
-        if re.findall("\d", letter):
-            return "s1"
-        elif re.findall("\w", letter):
-            return "s2"
-        else:
-            return "err"
+#     if state == "s1":
+#         if re.findall("\d", letter):
+#             return "s1"
+#         elif re.findall("\w", letter):
+#             return "s2"
+#         else:
+#             return "err"
 
-    if state =="s2":
-        if re.findall("\w", letter):
-            return "s2"
-        elif re.findall("\d", letter):
-            return "s1"
-        else:
-            return "err"
-
-
-if __name__ == "__main__":
-    c = DeterministicFiniteAutomaton(["a" , "b" , "c"],["s0","s1","s2","s3","s4","s5","s6","s7","s8","s9"] , funcao_de_transicao , "s0" , ["s2"])
+#     if state =="s2":
+#         if re.findall("\w", letter):
+#             return "s2"
+#         elif re.findall("\d", letter):
+#             return "s1"
+#         else:
+#             return "err"
 
 
-    # print(c.transition_function("s0","c"))
-    # print(c.transition_function("s0","9"))
-    print(c.lexemeVerify("abc"))
+# if __name__ == "__main__":
+#     c = DeterministicFiniteAutomaton(["a" , "b" , "c"],["s0","s1","s2","s3","s4","s5","s6","s7","s8","s9"] , funcao_de_transicao , "s0" , ["s2"])
+
+
+#     # print(c.transition_function("s0","c"))
+#     # print(c.transition_function("s0","9"))
+#     print(c.lexemeVerify("abc"))
 
